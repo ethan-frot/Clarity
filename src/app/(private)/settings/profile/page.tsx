8efa5b392827d90b9749eefa97755672984618b6
Ethan Frot <contact@ethanfrot.com>
2025-12-06 17:07:21 +0100

    ✨ US-15a : Add settings profile page

---
+++ src/app/(private)/settings/profile/page.tsx
@@ -0,0 +1,9 @@
+import { UpdateProfileForm } from '@/module/user/updateUserProfile/ui/UpdateProfileForm';
+
+export default function ProfileSettingsPage() {
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <UpdateProfileForm />
+    </div>
+  );
+}
